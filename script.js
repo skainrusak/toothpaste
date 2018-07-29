d911923a4f4ee063d4da593d8945be4fe628a2fc
genvarek <genvarek@gmail.com>
2018-07-30 01:33:12 +0600

    Added footer, fixed scripts

---
+++ script.js
@@ -0,0 +1,11 @@
+let myImgs = document.querySelectorAll('img')
+
+
+function on() {
+    document.getElementById("overlay").style.display = "block"
+}
+            
+function off() {
+    document.getElementById("overlay").style.display = "none"
+}
+
